6fa3e649b119bf50acdf654b729e35f909e06475
Caio Carvalho <kaiogesteira@gmail.com>
2024-07-23 17:59:38 -0300

    feat: finished project

--- src/routes/oauth-router.ts
+++ src/routes/oauth-router.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-
-
+import { getOauth } from '@/controllers/oauth-controller';
 const oauthRouter = Router();
 
-oauthRouter.get('/oauth/callback',);
+oauthRouter.get('/oauth/callback', getOauth);
 
 export { oauthRouter };
